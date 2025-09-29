32372157c97e16237dd3c6299301b3080bec60f8
Steve_Loic <tchoumisteve@gmail.com>
2025-09-30 00:05:56 +0200

    fix the test

--- src/test/java/com/steve/book/bookRespositoryTest/BookRepositoryTest.java
+++ src/test/java/com/steve/book/bookRespositoryTest/BookRepositoryTest.java
@@ -9,14 +9,18 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.utility.TestcontainersConfiguration;
 
 import java.util.Arrays;
 import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class BookRepositoryTest  {
 
     @Autowired
