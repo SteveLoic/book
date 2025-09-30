232fadf89cc6a71c8771fc5cbc65a5faca102eb5
Steve_Loic <tchoumisteve@gmail.com>
2025-09-30 16:17:28 +0200

    add test and add reusable workflow

--- src/main/java/com/steve/book/book/BookRepository.java
+++ src/main/java/com/steve/book/book/BookRepository.java
@@ -2,5 +2,12 @@ package com.steve.book.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findBookByTitle(String title);
+
+    Book findByIsbn(int isbn);
+
 }
--- src/test/java/com/steve/book/bookRespositoryTest/BookRepositoryTest.java
+++ src/test/java/com/steve/book/bookRespositoryTest/BookRepositoryTest.java
@@ -2,19 +2,14 @@ package com.steve.book.bookRespositoryTest;
 
 import com.steve.book.book.Book;
 import com.steve.book.book.BookRepository;
-import com.steve.book.book.BookResponse;
-import com.steve.book.common.AbstractionBaseTest;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.testcontainers.utility.TestcontainersConfiguration;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,10 +21,13 @@ public class BookRepositoryTest  {
     @Autowired
     private BookRepository bookRepository;
 
+    private List<Book> initialBooks;
 
-    @Test
-    void givenBooksList_whenSave_ReturnAllTheBooks(){
-        List<Book> books = Arrays.asList(
+    @BeforeEach
+    void setUp() {
+        bookRepository.deleteAll();
+
+        initialBooks = Arrays.asList(
                 Book.builder()
                         .year(2000)
                         .title("spring boot with Steve")
@@ -42,17 +40,59 @@ public class BookRepositoryTest  {
                         .title("React Apps with Steve")
                         .description("Best practices to use React with github actions")
                         .author("steve")
-                        .isbn(1)
+                        .isbn(1344)
                         .build()
         );
 
+    }
+
+    @Test
+    void givenBooksList_whenSave_ReturnAllTheBooks(){
         //SAVE
-        List<Book> savedBook = bookRepository.saveAll(books);
+        List<Book> savedBook = bookRepository.saveAllAndFlush(initialBooks);
 
         //Fetch All the Books
         List<Book> foundBooks = bookRepository.findAll();
 
+        //
         Assertions.assertEquals(foundBooks.size(),savedBook.size());
+    }
+
+
+    @Test
+    void givenBooks_whenUpdate_ReturnUpdatedBooks(){
+        //SAVE
+        bookRepository.saveAllAndFlush(initialBooks);
+
+        //Find the book to be updated
+         Book bookToBeUpdated = bookRepository.findByIsbn(1344);
+
+        // Update values
+        bookToBeUpdated.setTitle("Spring Boot und React mit GitHub Pipeline");
+        bookToBeUpdated.setDescription("Best practices to use React with Spring Boot Pipeline");
+
+        // SAVE updated
+        Book updatedBook = bookRepository.saveAndFlush(bookToBeUpdated);
+
+        // VERIFY
+        Assertions.assertEquals("Spring Boot und React mit GitHub Pipeline", updatedBook.getTitle());
+        Assertions.assertEquals("Best practices to use React with Spring Boot Pipeline", updatedBook.getDescription());
+    }
+
+
+    @Test
+    void givenBooks_whenDelete_ReturnDeletedBook(){
+        //SAVE
+        bookRepository.saveAllAndFlush(initialBooks);
+
+        //Find the book to be updated
+        bookRepository.delete(bookRepository.findByIsbn(1344));
+
+        // Retrieve all Book
+        List<Book> allBooks = bookRepository.findAll();
+
+        Assertions.assertEquals(allBooks.size(),1);
+
     }
 
 }
