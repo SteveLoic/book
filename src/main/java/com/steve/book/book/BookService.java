5a413524eb89efa1062b0a7671a7eb9b3ea9604a
Steve_Loic <tchoumisteve@gmail.com>
2025-10-16 14:30:00 +0200

    add some small changes to test container im backend

---
+++ src/main/java/com/steve/book/book/BookService.java
@@ -0,0 +1,22 @@
+package com.steve.book.book;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
+@Service
+@Slf4j
+public class BookService {
+}
+
+
+
+
